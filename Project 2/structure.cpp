7b2a2a1ba45ea13d85cd6a3de48980057b14b3f2
katlew3 <katlew3@users.noreply.github.com>
2015-03-30 08:39:07 -0400

    Update structure.cpp

--- Project 2/structure.cpp
+++ Project 2/structure.cpp
@@ -32,3 +32,42 @@ Haptic haptic[7] = {
 	{ center, center },
 };
 
+void createObjects(void) {
+	baseSize = loadObject("models/base.obj", red, Vertices, Indices, 23);
+	createVAOs(Vertices, Indices, 23);
+	haptic[0].translate = vec3(0.0f, 0.4f, 0.0f);
+}
+
+void renderScene(void) {
+	glUseProgram(programID);
+	{
+		vec3 lightPos = vec3(4,4,4);
+		mat4x4 ModelMatrix = mat4(1.0);
+		/* pass extra data to a shader for a specific primitive */
+		glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
+
+		/*Uniform*/
+		glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &gViewMatrix[0][0]);
+		glUniformMatrix4fv(ProjMatrixID, 1, GL_FALSE, &gProjectionMatrix[0][0]);
+		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+		/* Draw Objects */
+		/* Base - 23 */
+		ModelMatrix = translate(ModelMatrix, haptic[0].translate);
+		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+		glBindVertexArray(VertexArrayId[23]);
+		glDrawElements(GL_TRIANGLES, baseSize * 4, GL_UNSIGNED_SHORT, (void*)0);
+
+		/* Pen - 28 */
+		ModelMatrix = translate(ModelMatrix, vec3(0.0f, 0.6f, 0.0f));
+		ModelMatrix = rotate(ModelMatrix, haptic[5].rotate[2], vec3(0, 0, 1));
+		ModelMatrix = rotate(ModelMatrix, haptic[5].rotate[1], vec3(0, 1, 0));
+		ModelMatrix = rotate(ModelMatrix, haptic[5].rotate[0], vec3(1, 0, 0));
+		ModelMatrix = translate(ModelMatrix, haptic[5].translate);
+		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+		glBindVertexArray(VertexArrayId[28]);
+		glDrawElements(GL_TRIANGLES, penSize * 4, GL_UNSIGNED_SHORT, (void*)0);
+	}
+}
